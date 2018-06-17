3e515ed2f1640322d40dbb04bd6a9dc56bffcf07
nikita <nikita45454@gmail.com>
2018-06-18 01:10:36 +0500

    footer

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -25,7 +25,7 @@
                                             {{$categorie->name}}
                                         </div>
                                     </div>
-                                    <img src="{{url($categorie->img)}}" alt="">
+                                    <img src="{{url('public' . $categorie->img)}}" alt="">
                                 </a>
                           </div>
                           @endforeach
